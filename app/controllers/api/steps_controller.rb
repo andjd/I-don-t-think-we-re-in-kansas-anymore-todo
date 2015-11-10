490c2d60297ac595a40ca58ce06dd2d5bae26d58
App Academy Student <appacademy@Apps-Mac-mini-37.local>
2015-11-10 17:56:27 -0500

    tidbits

--- app/controllers/api/steps_controller.rb
+++ app/controllers/api/steps_controller.rb
@@ -1,16 +1,26 @@
 class Api::StepsController < ApplicationController
   def index
-    steps = Step.find_by_to_do_id(params[to_do_id])
+    steps = Step.find_by_to_do_id(params[:to_do_id])
     render json: steps
   end
 
   def create
-    to_do = ToDo.find(params[to_do_id])
+    to_do = ToDo.find(params[:to_do_id])
     step = to_do.steps.create!(step_params)
     render json: step
   end
 
-  def
+  def destroy
+    step = Step.find(params[:id])
+    step.destroy
+    render text: "it is done."
+  end
+
+  def update
+    step = Step.find(params[:id])
+    step.update!(step_params)
+    render json: step
+  end
 
 private
   def step_params
